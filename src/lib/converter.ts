dada5f3df6d5b0a01b3ffde094c3019a661fb630
Shihab Mahamud <shihab4t@gmail.com>
2025-03-03 05:26:08 +0600

    converter

--- src/lib/converter.ts
+++ src/lib/converter.ts
@@ -1,14 +1,18 @@
+// import { toast } from "@/lib/toast";
+
+import HTMLToJSX from "htmltojsx";
+
+const htmltojsx = new HTMLToJSX({ createClass: false });
+
 export const converter = (html: string) => {
-    return html;
-    // try {
-    //     const result = html;
-    //     setJsx(result);
-    //     toast.success("Conversion Complete", {
-    //         description: "HTML has been converted to JSX."
-    //     });
-    // } catch (error) {
-    //     toast.error("Conversion Error", {
-    //         description: "There was an error converting your HTML."
-    //     });
-    // }
+    try {
+        const jsx = htmltojsx.convert(html);
+        console.log(jsx);
+        // toast.success("Conversion Complete", { description: "HTML has been converted to JSX." });
+        return jsx;
+    } catch (error) {
+        // toast.error("Conversion Error", {
+        //     description: "There was an error converting your HTML."
+        // });
+    }
 };
