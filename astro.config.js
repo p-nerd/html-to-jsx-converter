435320ec868a38029da65d42e44471206c2486a4
Shihab Mahamud <shihab4t@gmail.com>
2025-03-03 16:14:06 +0600

    add sitemap intregration

--- astro.config.js
+++ astro.config.js
@@ -1,14 +1,14 @@
-// @ts-check
 import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 import vercel from "@astrojs/vercel";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
+    site: "https://html-to-jsx-converter.vercel.app",
     output: "static",
-    integrations: [react()],
     adapter: vercel({ webAnalytics: { enabled: true } }),
+    integrations: [react(), sitemap()],
     vite: { plugins: [tailwindcss()] }
 });
