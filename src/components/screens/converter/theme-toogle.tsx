728030fba0bfff925c9b71a7e05fd4958ebefb42
Shihab Mahamud <shihab4t@gmail.com>
2025-03-03 16:33:04 +0600

    add trainsiton

--- src/components/screens/converter/theme-toogle.tsx
+++ src/components/screens/converter/theme-toogle.tsx
@@ -54,7 +54,12 @@ const ThemeToogle = () => {
     };
 
     return (
-        <Button variant="outline" size="icon" onClick={toggleMode} className="cursor-pointer">
+        <Button
+            variant="outline"
+            size="icon"
+            onClick={toggleMode}
+            className="cursor-pointer transition-all duration-300"
+        >
             <SunIcon className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
             <MoonIcon className="absolute h-5 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
             <span className="sr-only">Toggle theme</span>
